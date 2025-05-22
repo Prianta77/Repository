07370f63948147985f55e7d440b93e93a0ef1d25
Prianta77 <priantadelvan77@gmail.com>
2025-05-22 09:11:21 +0700

    App.py

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title("Hello, world")
+st.title("Hello, World")
 st.write("Ini adalah aplikasi streamlit sederhana")
+
+st.set_page_config(page_title="Hello World App", layout="centered")
