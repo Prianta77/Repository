56d9504d714e7955c367a244eec7104a3f221b09
Prianta77 <priantadelvan77@gmail.com>
2025-06-18 16:27:48 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,18 +1,8 @@
 import streamlit as st
-import pandas as pd
-import pickle
-import joblib
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-st.set_page_config(page_title="Buzzer Twitter Dashboard App", layout="centered")
-st-sidebar.header("Dashboard")
 
 
-st.title("Selamat Datang di Aplikasi Streamlit Sederhana")
-st.write("Aplikasi ini dibuat untuk memenuhi tugas projek data mining")
-
-#Load Dataset
-df = pd.read_csv("Model/twitter_training.csv")
-
-#Tampilkan Dataframe
+st.title("Anggota Kelompok 14")
+st.write("Prianta Delvan Dwiradya (2304030007)")
+st.write("Yuthi' Callista Arya Khozana (2304030021)")
+st.write("Chika Ayu Oktafianita (2304030023)")
+st.write("Ine Febrianti (4101422087)")
